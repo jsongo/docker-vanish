df0bf347fd894577c65b242bec6a32f7b70bd398
jsongo <jsongo@qq.com>
2017-11-09 14:05:00 +0800

    update

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ substitute-env-vars.sh /etc/varnish /etc/varnish/default.vcl.tmpl
 
 # Start varnish and log
 varnishd -f /etc/varnish/default.vcl -s file,/var/lib/varnish/varnish_storage.bin,${CACHE_SIZE} -T 0.0.0.0:${VARNISH_ADMIN_PORT} -a 0.0.0.0:${VARNISH_PORT} ${VARNISHD_PARAMS}
+nginx
 varnishlog
